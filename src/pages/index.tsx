93ab5fecf85ef576e9a1211c0aa1ea86c83a718b
kriszi999 <o.kriszi99@gmail.com>
2023-02-21 14:52:40 +0100

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ const Home: NextPage = () => {
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
-            <AuthShowcase />
+            {<AuthShowcase />}
           </div>
         </div>
       </main>
@@ -63,7 +63,7 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
